68bca4da9308a566302c9f4b2679f3f50ccc6b15
vibharaj20 <63732535+vibharaj20@users.noreply.github.com>
2020-04-25 12:05:31 +0530

    Add files via upload

---
+++ pgm.py
@@ -0,0 +1,32 @@
+mydict = {"hello" : "world", "speckbit" : "self-learning", "life" : "meaning"}
+value_to_find=input("enter the value")
+for key,value in mydict.items():
+	if value==value_to_find:
+		print(key)
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+    
+
+		
+		
+
+
+
+	
+
+	
+
