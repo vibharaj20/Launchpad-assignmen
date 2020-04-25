84d63a260ddad0678e9169aa39c3d545509f8c97
vibharaj20 <63732535+vibharaj20@users.noreply.github.com>
2020-04-25 15:29:22 +0530

    Add files via upload

---
+++ assign4.py
@@ -0,0 +1,34 @@
+txt="TheProgrammer'sGuideToEffectiveLearning"
+out=txt[:3]+" "+txt[3:15]+" "+txt[15:20]+" "+txt[20:22]+" "+txt[22:32]+" "+txt[32:40]
+print(out)
+a="MyReviewOfTheNewPhone:SamsungGalaxyS9"
+out1=a[:2]+" "+a[2:8]+" "+a[8:10]+" "+a[10:13]+" "+a[13:16]+" "+a[16:21]+" "+a[21:22]+" "+a[22:29]+" "+a[29:36]
+print(out1)
+
+
+
+
+
+
+
+		
+
+
+
+
+
+
+	
+
+
+    
+
+		
+	
+
+
+
+	
+
+	
+
